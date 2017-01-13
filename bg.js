1ddc06a26800ad0842f3c206fd665a7abd224fec
Sebastian Waisbrot <seppo0010@gmail.com>
2017-01-13 16:33:32 -0300

    add more memes

--- bg.js
+++ bg.js
@@ -18,5 +18,53 @@ chrome.runtime.onMessageExternal.addListener(function(request, sender, sendRespo
             width: 480,
             height: 480,
         },
+        {
+            text: 'pelotudo',
+            url: chrome.extension.getURL('images/ahilotenesalpelotudo.jpg'),
+            width: 480,
+            height: 480,
+        },
+        {
+            text: 'cancha',
+            url: chrome.extension.getURL('images/andaalacanchabobo.jpg'),
+            width: 480,
+            height: 480,
+        },
+        {
+            text: 'decorado',
+            url: chrome.extension.getURL('images/eldecoradosecalla.jpg'),
+            width: 480,
+            height: 480,
+        },
+        {
+            text: 'tapado',
+            url: chrome.extension.getURL('images/forttapado.jpg'),
+            width: 480,
+            height: 480,
+        },
+        {
+            text: 'hacker',
+            url: chrome.extension.getURL('images/hacker.jpg'),
+            width: 480,
+            height: 480,
+        },
+        {
+            text: 'fracasado',
+            url: chrome.extension.getURL('images/jorgesosunfracasado.jpg'),
+            width: 480,
+            height: 480,
+        },
+        {
+            text: 'poor',
+            url: chrome.extension.getURL('images/stopbeingpoor.jpg'),
+            width: 480,
+            height: 480,
+        },
+        {
+            text: 'empanadas',
+            url: chrome.extension.getURL('images/tresempanadas.jpg'),
+            width: 480,
+            height: 480,
+        },
     ]);
 });
